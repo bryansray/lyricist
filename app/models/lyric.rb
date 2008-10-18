a70cf96fe609066043fb89a22a45571cc97fd9cb
Bryan Ray <bryansray@gmail.com>
2008-10-18 05:16:08 -0500

    Had to tell this relationship what the foreign key was - thought it would have inferred it from the class_name, but I guess not

--- app/models/lyric.rb
+++ app/models/lyric.rb
@@ -1,6 +1,6 @@
 class Lyric < ActiveRecord::Base
   validates_presence_of :text
   
-  belongs_to :owner, :class_name => 'User'
+  belongs_to :owner, :class_name => 'User', :foreign_key => 'user_id'
   belongs_to :song
 end
