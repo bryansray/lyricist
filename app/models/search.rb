fd6423eb8bad2cf696a1bbefe14cd3179e2d8d44
paul <paul@intridea.com>
2008-10-18 11:14:06 -0400

    Fulltext search DOES work.  It's just not playing nicely with rspec.

--- app/models/search.rb
+++ app/models/search.rb
@@ -8,8 +8,9 @@ class Search < ActiveRecord::Base
       keywords.split.each do |keyword|
         results[:artists] += Artist.find(:all, :conditions => [ "title LIKE ?", "%#{keyword}%" ])
         results[:songs]   += Song.find(:all, :conditions => [ "title LIKE ?", "%#{keyword}%" ])
-        results[:lyrics]  += Lyric.find(:all, :conditions => [ "text LIKE ?", "%#{keyword}%"])
+        results[:lyrics]  += Lyric.find(:all, :conditions => [ "text LIKE ?", "%#{keyword}%"]) # This line should be removed in the future.  It's here as a hack around rspec and acts_as_fulltextable not playing nicely together.  Yet.
       end
+      results[:lyrics] += Lyric.find_fulltext keywords
       results.values.map(&:uniq!)
     end
   end
