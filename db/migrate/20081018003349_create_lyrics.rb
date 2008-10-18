19466341a283e12009a4dbf6e9b8c0918db7c1f3
Bryan Ray <bryansray@gmail.com>
2008-10-17 19:46:46 -0500

    Initial models created (album, artist, lyric, song).

---
+++ db/migrate/20081018003349_create_lyrics.rb
@@ -0,0 +1,13 @@
+class CreateLyrics < ActiveRecord::Migration
+  def self.up
+    create_table :lyrics do |t|
+      t.text :text
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :lyrics
+  end
+end
