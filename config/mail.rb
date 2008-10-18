a2d58775b8c769f8dd90b54a72bf3eed77ac6d2c
Raymond Law <rayvinly@gmail.com>
2008-10-18 13:09:21 -0400

    Added GMail stuff

---
+++ config/mail.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :tls => true,
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :domain => 'lyricistapp.com',
+  :authentication => :plain,
+  :user_name => 'mailer@lyricistapp.com',
+  :password => '9468C6'
+}
