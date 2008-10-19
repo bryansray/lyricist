cc6f3f6d251ab3b88c89847173ccf246401d1059
Bryan Ray <bryansray@gmail.com>
2008-10-18 23:56:18 -0500

    Added an association to the Album model and a little cleanup here and there.

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,5 @@
 class Album < ActiveRecord::Base
   belongs_to :artist
+  
+  has_many :songs
 end
--- db/migrate/20081018003425_create_songs.rb
+++ db/migrate/20081018003425_create_songs.rb
@@ -2,6 +2,7 @@ class CreateSongs < ActiveRecord::Migration
   def self.up
     create_table :songs do |t|
       t.integer :artist_id, :album_id
+      t.integer :track
       t.string :title
 
       t.timestamps
