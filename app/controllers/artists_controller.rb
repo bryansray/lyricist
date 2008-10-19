b8998fe244c81e5e64060c918b863ac16218d6d9
Raymond Law <rayvinly@gmail.com>
2008-10-19 13:50:33 -0400

    artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,6 @@
 class ArtistsController < ApplicationController
+  # GET /artists
+  # GET /artists.xml
   def index
     @artists = Artist.find(:all).paginate(:page => params[:page])
 
@@ -9,27 +11,76 @@ class ArtistsController < ApplicationController
     end
   end
 
+  # GET /artists/1
+  # GET /artists/1.xml
   def show
-    @artist = Artist.find params[:id]
-    
+    @artist = Artist.find(params[:id])
+
     respond_to do |format|
-      format.html
+      format.html # show.html.erb
+      format.xml  { render :xml => @artist }
     end
   end
 
-  def edit
-  end
-
+  # GET /artists/new
+  # GET /artists/new.xml
   def new
+    @artist = Artist.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @artist }
+    end
   end
 
-  def update
+  # GET /artists/1/edit
+  def edit
+    @artist = Artist.find(params[:id])
   end
 
+  # POST /artists
+  # POST /artists.xml
   def create
+    @artist = Artist.new(params[:artist])
+
+    respond_to do |format|
+      if @artist.save
+        flash[:notice] = 'Artist was successfully created.'
+        format.html { redirect_to(@artist) }
+        format.xml  { render :xml => @artist, :status => :created, :location => @artist }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @artist.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
+  # PUT /artists/1
+  # PUT /artists/1.xml
+  def update
+    @artist = Artist.find(params[:id])
+
+    respond_to do |format|
+      if @artist.update_attributes(params[:artist])
+        flash[:notice] = 'Artist was successfully updated.'
+        format.html { redirect_to(@artist) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @artist.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /artists/1
+  # DELETE /artists/1.xml
   def destroy
-  end
+    @artist = Artist.find(params[:id])
+    @artist.destroy
 
+    respond_to do |format|
+      format.html { redirect_to(artists_url) }
+      format.xml  { head :ok }
+    end
+  end
 end
