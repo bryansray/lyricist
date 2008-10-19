102e6d94125dac1e1073dc42d47fc9169ca75f9f
Bryan Ray <bryansray@gmail.com>
2008-10-18 23:36:51 -0500

    Layout changes and updated the amarok import.

--- db/migrate/lyricist.import.sql
+++ db/migrate/lyricist.import.sql
@@ -1,3 +1,5 @@
 insert into artists (id, name) select id, name from amarok.artist;
 insert into albums (id, name) select id, name from amarok.album;
 insert into songs (artist_id, album_id, title ) select artist.id, album.id, tags.title from amarok.tags inner join amarok.album on tags.album = album.id inner join amarok.artist on tags.artist = artist.id;
+	
+update lyricist_development.albums, amarok.tags, amarok.album set albums.artist_id = tags.artist where albums.name = album.name and album.id = tags.album and albums.name = album.name
